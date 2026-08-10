fbe1e7c413dd79cdde8c885e1a4280bd1c1764d7
RevisKor <revissskor@gmail.com>
2026-08-10 18:36:52 +0000

    feat: add login screen UI

---
+++ include/ui/login_screen.h
@@ -0,0 +1,9 @@
+#ifndef LOGIN_SCREEN_H
+#define LOGIN_SCREEN_H
+
+#include "../common.h"
+
+// Prints the top-level menu (log in / register / exit).
+void LoginScreen_Show(void);
+
+#endif // LOGIN_SCREEN_H
---
+++ src/ui/login_screen.c
@@ -0,0 +1,13 @@
+#include "../../include/ui/login_screen.h"
+
+void LoginScreen_Show(void) {
+    printf("\n");
+    printf("  CLINIC MANAGEMENT SYSTEM\n");
+    printf("  ------------------------\n");
+    printf("  [1] Log In to Your Account\n");
+    printf("  [2] Register New Account\n");
+    printf("  [3] Exit System\n");
+    printf("  ------------------------\n");
+    printf("\n");
+    printf("  Selection > ");
+}
